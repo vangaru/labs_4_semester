18cb1c0e8d42b2faa3ec81970894660dc9618337
tuesdaydude <ivanenko0607@gmail.com>
2021-04-03 12:32:53 -0800

    added lab 6 OSISP source

--- OSISP/lab6/src/lab.c
+++ OSISP/lab6/src/lab.c
@@ -1,84 +1,152 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <mqueue.h>
 #include <signal.h>
 
-#define MAX_SSIZE 100
+#define SERVER_QUEUE_NAME "/server"
+#define CLIENT_QUEUE_NAME "/client"
+#define QUEUE_PERMISSIONS 0660
+#define MAX_MESSAGES 10
+#define MAX_MESSAGE_SIZE 256
 #define SCOUNT 3
-#define MAX_MSG_IN_QUEUE 10
-#define MAX_MSG_SIZE 1024
-#define MQNAME "/msg"
+#define TRUE 1
+#define FALSE 0
 
-void handl_sigusr1() {
-  printf("SIGUSR1 HANDLER\n");
+size_t is_vowel(char c) {
+  if(c == 'a' || c == 'A' ||
+     c == 'e' || c == 'E' ||
+     c == 'i' || c == 'I' ||
+     c == 'o' || c == 'O' ||
+     c == 'u' || c == 'U' ||
+     c == 'y' || c == 'Y') {
+       return TRUE;
+     }
+  return FALSE;
+}
+
+void killer() {
+  write(STDOUT_FILENO, "They killed Kenny\n", 19);
 }
 
 int main() {
-  char ss[SCOUNT][MAX_SSIZE];
+  char input_strings[SCOUNT][MAX_MESSAGE_SIZE + 1];
 
-  printf("Enter first string: ");
-  fgets(ss[0], MAX_SSIZE, stdin);
-  printf("Enter second string: ");
-  fgets(ss[1], MAX_SSIZE, stdin);
-  printf("Enter third string: ");
-  fgets(ss[2], MAX_SSIZE, stdin);
+  for(size_t i = 0; i < SCOUNT; i++) {
+    printf("Enter %zu string : ", i + 1);
+    fgets(input_strings[i], MAX_MESSAGE_SIZE, stdin);
+    if ((strlen(input_strings[i]) > 0) && (input_strings[i][strlen (input_strings[i]) - 1] == '\n'))
+        input_strings[i][strlen (input_strings[i]) - 1] = '\0';
+  }
 
-  int pid = fork();
-  if(pid == -1) {
+  struct sigaction action = { 0 };
+  action.sa_handler = killer;
+  sigaction(SIGUSR1, &action, NULL);
+
+  pid_t pid;
+  if((pid = fork()) == -1) {
+    perror("Pid fork");
     return 1;
-  }
+  } else if(pid > 0) {
+    // Client process
+    struct mq_attr client_attr;
+    client_attr.mq_flags = 0;
+    client_attr.mq_maxmsg = MAX_MESSAGES;
+    client_attr.mq_msgsize = MAX_MESSAGE_SIZE;
+    client_attr.mq_curmsgs = 0;
 
-  if(pid == 0) {
-    // Child process
-    sleep(1);
-    kill(getppid(), SIGUSR1);
-    mqd_t rcv_hndl = mq_open(MQNAME, O_RDWR);
-    if(rcv_hndl < 0) {
-      printf("Error %d (%s) mq_open for receive\n");
-      exit(-1);
+    kill(pid, SIGUSR1);
+
+    mqd_t qd_send_to_server, qd_receive_from_server;
+
+    if((qd_send_to_server = mq_open(SERVER_QUEUE_NAME, O_WRONLY)) == -1) {
+      perror("Client: mq_open(server)");
+      exit(1);
     }
-    printf("\nReceiving messages\n");
-    printf("Opened mqd_t of %d \n", rcv_hndl);
-    printf("Receiving mqd_t of %d \n\n", rcv_hndl);
 
-    char buf[MAX_MSG_SIZE];
-    for(int i = 0; i < SCOUNT; i++) {
-      int rec_res = mq_receive(rcv_hndl, buf, sizeof(buf), NULL);
-      if(rec_res < 0) {
-        printf("Error %d (%s) mq_receive", errno, strerror(errno));
-        exit(-1);
+    for(size_t i = 0; i < SCOUNT; i++) {
+      if(mq_send(qd_send_to_server, input_strings[i], strlen(input_strings[i]), 0) == -1) {
+        perror("Client: Not able to send message to server");
+        exit(1);
       }
-      printf("Received: %s \n", buf);
+      printf("Client (%d): message sent to server: %s\n", getpid(), input_strings[i]);
     }
+
+
+    if((qd_receive_from_server = mq_open(CLIENT_QUEUE_NAME, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &client_attr)) == -1) {
+      perror("Client: mq_open(client)");
+      exit(1);
+    }
+
+    ssize_t bts_read;
+    char buf[MAX_MESSAGE_SIZE + 1];
+    if((bts_read = mq_receive(qd_receive_from_server, buf, MAX_MESSAGE_SIZE, NULL)) == -1) {
+      perror("Client: mq_receive");
+      exit(1);
+    }
+    buf[bts_read] = '\0';
+
+    //sleep(1);
+    printf("Client (%d): result message received from server: %s\n", getpid(), buf);
+
+
   } else {
-    // Parent process
-    struct mq_attr mq;
-    mq.mq_maxmsg = MAX_MSG_IN_QUEUE;
-    mq.mq_msgsize = MAX_MSG_SIZE;
-    printf("Open message queue\n");
-    mqd_t snd_hndl = mq_open(MQNAME, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR, &mq);
-    if(snd_hndl < 0) {
-      printf("Error %d (%s) mq_open for send\n", errno, strerror(errno));
-      exit(-1);
+    // Server process
+    struct mq_attr server_attr;
+    server_attr.mq_flags = 0;
+    server_attr.mq_maxmsg = MAX_MESSAGES;
+    server_attr.mq_msgsize = MAX_MESSAGE_SIZE;
+    server_attr.mq_curmsgs = 0;
+
+    mqd_t qd_receive_from_client, qd_send_to_client;
+
+    if((qd_receive_from_client = mq_open(SERVER_QUEUE_NAME, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &server_attr)) == -1) {
+      perror("Server: mq_open(client)");
+      exit(1);
     }
-    printf("Opened mqd_t of %d\n", snd_hndl);
-    for(int i = 0; i < SCOUNT; i++) {
-      int snd_res = mq_send(snd_hndl, ss[i], sizeof(ss[i]), 1);
-      if(snd_res < 0) {
-        printf("Error %d (%s) mq_send\n", errno, strerror(errno));
-        exit(-1);
+
+    char in_buffer[SCOUNT][MAX_MESSAGE_SIZE];
+    ssize_t bytes_read;
+    for(size_t i = 0; i < SCOUNT; i++) {
+      if((bytes_read = mq_receive (qd_receive_from_client, in_buffer[i], MAX_MESSAGE_SIZE, NULL)) == -1) {
+        perror("Server: mq_receive");
+        exit(1);
       }
-      printf("Send mqd_t of %d\n", snd_hndl);
+      in_buffer[i][bytes_read] = '\0';
+      printf("Server (%d): message received from client(%d): %s\n", getpid(), getppid(), in_buffer[i]);
     }
-    struct sigaction sa = { 0 };
-    sa.sa_handler = &handl_sigusr1;
-    sigaction(SIGUSR1, &sa, NULL);
 
-    wait(NULL);
+    size_t max_vowels = 0;
+    char res_string[MAX_MESSAGE_SIZE];
+
+    for(size_t i = 0; i < SCOUNT; i++) {
+      size_t tmp = 0;
+      for(size_t j = 0; j < strlen(in_buffer[i]); j++) {
+        if(is_vowel(in_buffer[i][j])) {
+          tmp++;
+        }
+      }
+      if(tmp > max_vowels) {
+        max_vowels = tmp;
+        strcpy(res_string, in_buffer[i]);
+        res_string[strlen(in_buffer[i])] = '\0';
+      }
+    }
+
+    printf("Server (%d): sending result to client (%d): %s\n", getpid(), getppid(),res_string);
+    if((qd_send_to_client = mq_open(CLIENT_QUEUE_NAME, O_WRONLY)) == -1) {
+      perror("Server: mq_open(client)");
+      exit(1);
+    }
+
+    if(mq_send(qd_send_to_client, res_string, strlen(res_string), 0) == -1) {
+      perror("Server: Not able to send message to client");
+      exit(1);
+    }
   }
-
-
   return 0;
 }
